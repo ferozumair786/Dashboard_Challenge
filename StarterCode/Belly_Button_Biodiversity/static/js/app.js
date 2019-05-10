8b9596b6aa1004c1d5e6387414255566a0d539a7
Umair Khakoo <ferozumair786@gmail.com>
2019-05-09 19:22:05 -0500

    made some progress

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,30 +1,50 @@
-function buildMetadata(sample) {
+async function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
+    if (sample != undefined)
+    {let metaURL =  `/metadata/${sample}`
+    console.log(sample)
+    console.log(metaURL)
+    let metaData = await d3.json(metaURL);
+    console.log(metaData)
+    // // Use d3 to select the panel with id of `#sample-metadata`
+    var panel = d3.select('#sample-metadata');
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    
+    Object.entries(metaData).forEach(([key, value]) => {
+      panel.append("h6").html(`<b>${key}:</b> ${value}`)
+    });}
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
+buildMetadata();
 
-function buildCharts(sample) {
+async function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+    if (sample != undefined )
+    {let sampleURL = `/samples/${sample}`
+    console.log(sample)
+    console.log(sampleURL)
+    let sampleData = await d3.json(sampleURL);
+    console.log(sampleData)
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // top10 = sampleData.slice(0,10)
+    // console.log(`Top 10: ${top10}`)
     // otu_ids, and labels (10 each).
-}
+    }
+};
+buildCharts();
 
 function init() {
   // Grab a reference to the dropdown select element
