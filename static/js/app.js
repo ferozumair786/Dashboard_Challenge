fc88f7b5b6b1a014b745dfd25de53184dd500ea1
Umair Khakoo <ferozumair786@gmail.com>
2019-05-11 10:05:03 -0500

    added comma

--- static/js/app.js
+++ static/js/app.js
@@ -75,7 +75,7 @@ async function buildCharts(sample) {
       type: "pie",
       name: "Pie Chart",
       hovertext: sampleData.otu_labels.slice(0,10),
-      hoverinfo: 'hovertext'
+      hoverinfo: 'hovertext',
       values: sampleData.sample_values.slice(0,10),
       labels: sampleData.otu_ids.slice(0,10)
     }
